e1694c4a5f8c900db4ca23c075f4db9c40decbc9
andrewcampbell <awr.campbell@gmail.com>
2021-02-15 16:45:13 -0500

    added event listener to mobile hamburger menu icon

---
+++ script/script.js
@@ -0,0 +1,9 @@
+
+const navButton = document.querySelector('.fa-bars');
+// put an event listener on the nav button
+navButton.addEventListener('click', function(event){
+    console.log(event);
+    console.log('clicked');
+    
+
+})
